1c0127836cd676a2ed6d7c00524b1f86c78af8e8
xct3583 <dorothy.chen@cunamutual.com>
2017-11-13 12:11:24 -0600

    Add R/*

---
+++ R/fars-read.R
@@ -0,0 +1,23 @@
+#' Read in a data set
+#'
+#' Reads a data file from the current working directory and converts it to a
+#' dataframe if a data file exist. Returns an error if the file doesn't exists, and
+#' show an error message "file (file name) does not exist"
+#'
+#' @param filename a text with ".csv" or ".csv.bz2" as an extension
+#' @return a dataframe of raw data
+#' @examples
+#' \dontrun{
+#' fars_read("data/accident_2013.csv.bz2")
+#' }
+#' @importFrom dplyr tbl_df
+#' @importFrom readr read_csv
+#' @export
+fars_read <- function(filename) {
+  if(!file.exists(filename))
+    stop("file '", filename, "' does not exist")
+  data <- suppressMessages({
+    readr::read_csv(filename, progress = FALSE)
+  })
+  dplyr::tbl_df(data)
+}

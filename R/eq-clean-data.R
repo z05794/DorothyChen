0f79ed70016456f128fea528bb9501e805082f72
Chen <Dorothy.Chen@cunamutual.com>
2018-01-20 16:23:45 -0600

    More changes

--- R/eq-clean-data.R
+++ R/eq-clean-data.R
@@ -4,7 +4,7 @@
 #' columns
 #' @examples
 #' \dontrun{
-#' df <- readr::read_delim("exdata/signif.txt", delim = "\t")
+#' data <- readr::read_delim("signif.txt", delim = "\t")
 #' library(dplyr)
 #' library(stringr)
 #' library(lubridate)
@@ -13,24 +13,46 @@
 #' @importFrom dplyr %>% mutate select
 #' @importFrom lubridate ymd
 #' @importFrom stringr str_to_title str_pad
-#' @importFrom stats df
 #' @export
 
-eq_clean_data <- function(df) {
-  clean_data <- df %>%
-    dplyr::select(COUNTRY, LOCATION_NAME, LATITUDE, LONGITUDE, YEAR, MONTH,
-                  DAY, HOUR,EQ_MAG_ML, DEATHS, TOTAL_DEATHS, EQ_PRIMARY) %>%
-    dplyr::mutate(LOCATION_NAME = stringr::str_to_title(gsub("[^;\n]+[:]", "",
-                                                             LOCATION_NAME)),
-                  LATITUDE = as.numeric(LATITUDE),
-                  LONGITUDE = as.numeric(LONGITUDE),
-                  DEATHS = as.numeric(DEATHS), 
-                  year = stringr::str_pad(as.character(abs(YEAR)), width = 4,
-                                          side = "left", pad = "0"),
-                  date_paste = paste(year, MONTH, DAY, sep = "-"),
-                  datetime = lubridate::ymd(date_paste, truncated = 2)) %>%
-    dplyr::select(COUNTRY, LOCATION_NAME, LATITUDE, LONGITUDE,year,
-                  EQ_MAG_ML, DEATHS, EQ_PRIMARY,  datetime)
-  names(clean_data) <- tolower(names(clean_data))
-  return(clean_data)
+eq_clean_data <- function(data) {
+  data <- data %>%
+    dplyr::mutate_(
+      year_fix = ~stringr::str_pad(as.character(abs(YEAR)), width = 4,
+                                   side = "left", pad = "0"),
+      date_paste = ~paste(year_fix, MONTH, DAY, sep = "-"),
+      datetime = ~lubridate::ymd(date_paste, truncated = 2)) %>%
+    dplyr::select_(quote(-year_fix), quote(-date_paste))
+  
+  lubridate::year(data$DATE) <- data$YEAR
+  
+  data <- data %>%
+    dplyr::mutate_(LATITUDE = ~as.numeric(LATITUDE),
+                   LONGITUDE = ~as.numeric(LONGITUDE))
+  
+  data <- eq_location_clean(data)
+  names(data) <- tolower(names(data))
+  return(data)
+}
+
+#' Cleans earthquake location data
+#'
+#' @param data A data frame with raw data obtained from NOAA website
+#' @return A data frame with cleaned LOCATION_NAME column
+#' @examples
+#' \dontrun{
+#' data <- readr::read_delim("signif.txt", delim = "\t")
+#' clean_data <- eq_location_clean(data)
+#' }
+#' @importFrom dplyr %>% mutate
+#' @importFrom stringr str_replace str_trim str_to_title
+#' @export
+eq_location_clean <- function(data) {
+  data <- data %>%
+    dplyr::mutate_(LOCATION_NAME = ~LOCATION_NAME %>%
+                     stringr::str_replace(paste0(COUNTRY, ":"), "") %>%
+                     stringr::str_trim("both") %>%
+                     stringr::str_to_title())
+  names(data) <- tolower(names(data))
+  return(data)
 }
